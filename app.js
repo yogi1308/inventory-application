56dfe6b9ea3835069b04f36373cca03a2b025aa9
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-09-19 20:23:37 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const path = require("node:path")
+
+const app = express()
+
+app.get("/", (req, res) => {res.send("Hello World")})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Listening on port ${PORT}!`);
+});
